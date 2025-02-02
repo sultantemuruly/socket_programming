43e8e0d3c2411d754891532eeb783d3fad51f558
sultantemuruly <sultantemuruly@gmail.com>
2025-02-02 16:05:37 +0500

    Added constants.py to centralize configuration variables

--- client.py
+++ client.py
@@ -1,26 +1,5 @@
 import socket
-
-
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # Connect to an external server (doesn't actually send data)
-        s.connect(("8.8.8.8", 80))
-        ip_address = s.getsockname()[0]
-    except Exception:
-        ip_address = "127.0.0.1"
-    finally:
-        s.close()
-    return ip_address
-
-
-PORT = 5056
-SERVER = get_local_ip()
-ADDR = (SERVER, PORT)
-
-HEADER = 64
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
+from constants import *
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
